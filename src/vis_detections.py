4a35fcc4a2989993ecc01a86f59a765298ae5085
JoejynWan <joejyn@gmail.com>
2022-02-04 18:13:18 +0800

    Initial commit

--- src/vis_detections.py
+++ src/vis_detections.py
@@ -1,36 +1,60 @@
 import os
 import cv2
 import json
+from tqdm import tqdm
 import argparse
 import tempfile
 from pickle import TRUE
+from typing import Any
 
 # Functions imported from this project
 from shared_utils import delete_temp_dir, unique
 
 # Imported from Microsoft/CameraTraps github repository
 from visualization.visualize_detector_output import visualize_detector_output
+from visualization import visualization_utils as vis_utils
+from data_management.annotations.annotation_constants import (
+    detector_bbox_category_id_to_name)  # here id is int
 
-def create_save_paths(input_frames_anno_file, output_dir):
-    with open(input_frames_anno_file) as f:
+
+#%% Constants
+
+# Convert category ID from int to str
+DEFAULT_DETECTOR_LABEL_MAP = {
+    str(k): v for k, v in detector_bbox_category_id_to_name.items()
+}
+
+
+#%% Functions
+
+def load_detector_output(detector_output_path):
+    with open(detector_output_path) as f:
         detector_output = json.load(f)
     assert 'images' in detector_output, (
         'Detector output file should be a json with an "images" field.')
     images = detector_output['images']
 
+    detector_label_map = DEFAULT_DETECTOR_LABEL_MAP
+    if 'detection_categories' in detector_output:
+        print('detection_categories provided')
+        detector_label_map = detector_output['detection_categories']
+
+    return images, detector_label_map
+
+def find_unqiue_videos(images, output_dir):
+
     frames_paths = []
-    output_video_names = []
-    output_save_paths = []
+    video_save_paths = []
     for entry in images:
         frames_path = os.path.dirname(entry['file'])      
         frames_paths.append(frames_path)
-        output_frames_path = os.path.join(output_dir, frames_path)
-        output_video_names.append(os.path.basename(output_frames_path))
-        output_save_paths.append(os.path.dirname(output_frames_path))
+        video_save_paths.append(os.path.join(output_dir, os.path.dirname(frames_path)))
 
-    [os.makedirs(make_path, exist_ok=True) for make_path in unique(output_save_paths)]
+    unqiue_videos = unique(frames_paths)
 
-    return frames_paths, output_video_names, output_save_paths
+    [os.makedirs(make_path, exist_ok=True) for make_path in unique(video_save_paths)]
+
+    return unqiue_videos
 
 def frames_to_video(images, Fs, output_file_name, codec_spec = 'DIVX'):
     """
@@ -58,32 +82,75 @@ def frames_to_video(images, Fs, output_file_name, codec_spec = 'DIVX'):
     cv2.destroyAllWindows()
 
 
-def vis_detection_video(tempdir, detector_output_path, images_dir, output_video_file, confidence):
-    ## Rendering detections to images 
-    rendering_output_dir = os.path.join(tempdir, 'detection_frames')
-    
-    detected_frame_files = visualize_detector_output(
-        detector_output_path = detector_output_path,
-        out_dir = rendering_output_dir,
-        images_dir = images_dir,
-        confidence = confidence)
-    
-    frames_to_video(detected_frame_files, 30, output_video_file)
+def vis_detector_output(images, detector_label_map, images_dir, out_dir, confidence, output_image_width = 700):
+    """
+    Args: 
+    images = list, detection dictionary for each of the image frames, loaded from the detector_output .json file
+    detector_label_map = str, detector categories, loaded from the detector_output .json file
+    out_dir = str, temporary directory where annotated frame images will be saved
+    confidence = float, confidence threshold above which annotations will be rendered
+    """
 
-    delete_temp_dir(rendering_output_dir)
+    ## Arguments error checking
+    assert confidence > 0 and confidence < 1, (
+        f'Confidence threshold {confidence} is invalid, must be in (0, 1).')
+    
+    os.makedirs(out_dir, exist_ok=True)
+
+    num_saved = 0
+    annotated_img_paths = []
+    image_obj: Any  # str for local images, BytesIO for Azure images
+
+    for entry in images:
+        image_id = entry['file']
+        
+        if 'failure' in entry:
+            print(f'Skipping {image_id}, failure: "{entry["failure"]}"')
+            continue
+
+        image_obj = os.path.join(images_dir, image_id)
+        if not os.path.exists(image_obj):
+            print(f'Image {image_id} not found in images_dir; skipped.')
+            continue
+
+        # resize is for displaying them more quickly
+        image = vis_utils.resize_image(
+            vis_utils.open_image(image_obj), output_image_width)
+
+        vis_utils.render_detection_bounding_boxes(
+            entry['detections'], image, label_map=detector_label_map,
+            confidence_threshold=confidence)
+
+        for char in ['/', '\\', ':']:
+            image_id = image_id.replace(char, '~')
+        annotated_img_path = os.path.join(out_dir, f'anno_{image_id}')
+        annotated_img_paths.append(annotated_img_path)
+        image.save(annotated_img_path)
+        num_saved += 1
+
+    return annotated_img_paths
 
 
 def vis_detection_videos(tempdir, input_frames_anno_file, input_frames_base_dir, output_dir, confidence):
     
-    frames_paths, output_video_names, output_save_paths = create_save_paths(input_frames_anno_file, output_dir)
+    images, detector_label_map = load_detector_output(input_frames_anno_file)
 
-    for frame_path, save_path, video_name in zip(frames_paths, output_save_paths, output_video_names): 
+    unqiue_videos = find_unqiue_videos(images, output_dir)
+    rendering_output_dir = os.path.join(tempdir, 'detection_frames')
+
+    print('Rendering detections above a confidence threshold of {} for {} videos...'.format(
+        confidence, len(unqiue_videos)))
+    for unqiue_video in tqdm(unqiue_videos):
+        images_set = [s for s in images if unqiue_video in s['file']]
+
+        detected_frame_files = vis_detector_output(
+            images_set, detector_label_map, input_frames_base_dir, 
+            rendering_output_dir, confidence)
         
-        input_frames_dir = os.path.join(input_frames_base_dir, frame_path)
-        output_video_file = os.path.join(save_path, video_name)
+        output_video_file = os.path.join(output_dir, unqiue_video)
+        frames_to_video(detected_frame_files, 30, output_video_file)
 
-        vis_detection_video(tempdir, input_frames_anno_file, 
-                            input_frames_dir, output_video_file, confidence)
+        delete_temp_dir(rendering_output_dir)
 
 
 def main():
