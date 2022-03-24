235326b7e5d163f5374e5150b983003b861ec6cb
JoejynWan <joejyn@gmail.com>
2022-03-24 18:41:38 +0800

    Add manual_ID_csv

--- src/video_metadata.py
+++ src/video_metadata.py
@@ -1,39 +1,60 @@
 import os
-import ffmpeg
+import pandas as pd
 from datetime import datetime
+
+from shared_utils import make_output_path
 
 from detection.video_utils import find_videos
 
-input_dir = 'data/Round_14_January_2020'
 
-input_dir_full_paths = find_videos(input_dir, recursive=True)
-vids_relative_paths = [os.path.relpath(s,input_dir) for s in input_dir_full_paths]
-vids_relative_paths = [s.replace('\\','/') for s in vids_relative_paths]
+def manual_ID_csv(input_dir, csv_file):
 
-vid_rel_path = vids_relative_paths[0]
+    input_dir_full_paths = find_videos(input_dir, recursive=True)
+    vids_rel_paths = [os.path.relpath(s,input_dir) for s in input_dir_full_paths]
+    vids_rel_paths = [s.replace('\\','/') for s in vids_rel_paths]
 
-station_sampledate, species_abundance, vid_name = vid_rel_path.split("/")
-station, sampledate =  station_sampledate.split("_")
-species_details = species_abundance.split(" ")
-file = os.path.join(station_sampledate, vid_name).replace('\\','/')
+    true_vids = pd.DataFrame()
+    for vids_rel_path in vids_rel_paths:
 
+        station_sampledate, species_abundance, vid_name = vids_rel_path.split("/")
+        station, sampledate =  station_sampledate.split("_")
+        genus, _, species_quantity = species_abundance.partition(" ")
+        species, _, quantity_remarks = species_quantity.partition(" ")
+        quantity, _, remarks = quantity_remarks.partition(" ")
+        file = os.path.join(station_sampledate, vid_name).replace('\\','/')
 
-print(vids_relative_paths[0])
+        vid_full_path = os.path.join(input_dir, vids_rel_path)
+        creation_datetime = datetime.fromtimestamp(os.path.getctime(vid_full_path))
 
-vid_full_path = os.path.join(input_dir, vid_rel_path)
-creation_datetime = datetime.fromtimestamp(os.path.getctime(vid_full_path))
+        true_vid_dict = {
+            'UniqueFileName': file, 
+            'FileName': vid_name, 
+            'Station': station,
+            'SamplingDate': sampledate,
+            'Date': str(creation_datetime.date()),
+            'Time': str(creation_datetime.time()),
+            'DateTime': str(creation_datetime),
+            'Genus': str(genus),
+            'Species': str(species),
+            'ScientificName': str(genus + " " + species),
+            'Quantity': quantity,
+            'Remarks': remarks
+        }
+        
+        true_vid_pd = pd.DataFrame(true_vid_dict, index = [0])
+        true_vids = true_vids.append(true_vid_pd)
 
-true_list = {
-    'Station': station,
-    'SamplingDate': sampledate,
-    'Date': str(creation_datetime.date()),
-    'Time': str(creation_datetime.time()),
-    'DateTime': str(creation_datetime),
-    'File': file
-}
+    true_vids.to_csv(csv_file, index = False)
 
-print(true_list)
+def main():
 
+    os.makedirs(output_dir, exist_ok = True)
 
-# video = ffmpeg.probe(vid_full_path)
-# print(video)
+    manual_ID_csv(input_dir, csv_file)
+
+if __name__ == '__main__':
+    input_dir = 'data/20211119'
+    output_dir = 'results/20211119'
+    csv_file = make_output_path(output_dir, input_dir, "_manual_ID.csv")
+
+    main()
